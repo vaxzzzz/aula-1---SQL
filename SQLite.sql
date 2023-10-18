c1cb01d740d2002cf91303c8c1eeb87382fa193f
Victor Vaz <134503953+vaxzzzz@users.noreply.github.com>
2023-10-18 09:47:57 -0300

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,13 @@
+CREATE TABLE CLIENTE (
+ID_Cliente INT NOT	NULL,
+	Nome VARCHAR (60) NOT	NULL,
+  	CPF char (14)  NOT	NULL,
+  	RG cahr (12) NOT	NULL,
+  	Logradouro varchar (100) NOT NULL,
+  	CEP char (9) NOT	NULL,
+  	UF char (2) NOT	NULL,
+    Cidade varchar (30) NOT	NULL,
+  	Telefone char (15) NOT	NULL,
+  	Email varchar (355) NOT	NULL
+  	CNH char (9) NOT NULL,
+  	renda floalt (10) NULL);
